28d8ed9e2d177b8645aaa80adfa0ca197a4052cf
415858791@qq.com <415858791@qq.com>
2022-01-19 17:28:01 +0800

    Signed-off-by: 415858791@qq.com <415858791@qq.com>

---
+++ springmybatis/src/main/java/com/springmybatis/config/GeneratorCodeConfig.java
@@ -0,0 +1,26 @@
+package com.springmybatis.config;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+
+import java.util.Scanner;
+
+public class GeneratorCodeConfig {
+
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+
+
+}
---
+++ springmybatis/src/main/java/com/springmybatis/model/SysUser.java
@@ -0,0 +1,10 @@
+package com.springmybatis.model;
+
+import lombok.Data;
+
+@Data
+public class SysUser {
+    private int id;
+    private String username;
+    private String password;
+}
